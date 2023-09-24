c01b701d15ab2a444cbecd5916a0085bd82eb01a
takksakk <kzwkzw1818@gmail.com>
2023-09-24 17:25:33 +0900

    update

--- assets/global.js
+++ assets/global.js
@@ -1258,4 +1258,28 @@ class ProductRecommendations extends HTMLElement {
 
 customElements.define('product-recommendations', ProductRecommendations);
 
+document.addEventListener('DOMContentLoaded', function () {
 
+  // カラースウォッチまたは画像要素を取得
+  var variantChips = document.querySelectorAll('.card__variant_chips_image img');
+  
+  // 各要素に対してクリックイベントリスナーを設定
+  variantChips.forEach(function(chip) {
+      chip.addEventListener('click', function(e) {
+
+          // クリックされた要素のdata-productimage属性から画像URLを取得
+          var newImageUrl = e.currentTarget.getAttribute('data-productimage');
+
+          // 新しい画像URLを主要な商品画像のsrc属性に設定
+          var mainProductImage = document.querySelector('.main-product-image-selector'); // 主要な商品画像要素のセレクターを正確に指定してください。
+          if(mainProductImage) mainProductImage.setAttribute('src', newImageUrl);
+
+          // オプション：選択されたスウォッチまたは画像に対して「選択された」スタイルを設定
+          variantChips.forEach(function(chip) {
+              chip.classList.remove('selected'); // すべての要素から'selected'クラスを削除
+          });
+          e.currentTarget.classList.add('selected'); // クリックされた要素に'selected'クラスを追加
+      });
+  });
+
+});
